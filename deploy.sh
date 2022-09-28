76b1be1c5dbb735200b0152d5810cc57ce6d4c13
OneFlyingBanana <87445607+OneFlyingBanana@users.noreply.github.com>
2022-09-28 16:36:21 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# if you are deploying to https://<USERNt: :bearhug: :mgg: :sgj: AME>.github.io/<REPO>
+git push -f git@github.com:OneFlyingBanana/vue_cli_test.git master:gh-pages
 
 cd -
